a49cfc6bb3b09c159895c3cd555849e96905be45
alimukhtor <james1999007@gmail.com>
2022-02-04 18:04:13 +0100

    displaying weathing infos

--- weather-app/src/components/WeatherInfo.tsx
+++ weather-app/src/components/WeatherInfo.tsx
@@ -1,6 +1,7 @@
 import { Container, Row, Col, Form, Card, Button } from "react-bootstrap";
 import { IWeather } from "./types/weather";
 import { Forecast } from "./types/forecast";
+import logo from '../assets/images/pic.png'
 import {useState, useEffect} from 'react'
 interface WeatherInfoProps{
     weather: IWeather
@@ -8,15 +9,15 @@ interface WeatherInfoProps{
 
 
 const WeatherInfo =({weather}:WeatherInfoProps)=> {
-    const [forecast, setForecast] = useState<Forecast[]>([])
+    const [forecast, setForecast] = useState<Forecast["daily"]>([])
     const apiKey: any = "2e9b02965350b65e7874cee2f70d914e";
 
     const fetchForecast = async({weather}:WeatherInfoProps)=> {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.lat}&lon=${weather.lon}&exclude=minutely&appid=${apiKey}`)
         if(response.ok){
-            const data = await response.json()
+            const data = await response.json() as Forecast
             console.log("Forecast:", data);
-            let onlineForecast: Forecast[] = data.onlineForecast
+            let onlineForecast = data.daily
             setForecast(onlineForecast)
             
         }
@@ -27,15 +28,17 @@ const WeatherInfo =({weather}:WeatherInfoProps)=> {
     return(
         <>
         {
-            // forecast.map(fore => (
+            forecast.map(fore => (
+            <>
+            <Card.Img src={logo} className="fluid" style={{ objectFit: "cover" }} />
             <Card.Body>
               <Card.Title>{weather.name}</Card.Title>
               <Card.Text>
-                {/* {fore.current.weather[0].main} */}
+                {fore.weather[0].main}
               </Card.Text>
             </Card.Body>
-
-            // ))
+            </>
+             ))
         }
         </>
     )
