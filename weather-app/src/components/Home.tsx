69ff6d86aedb5c1a7a13374e7e02fdf46be7899c
alimukhtor <james1999007@gmail.com>
2022-02-04 10:22:30 +0100

    added input field

--- weather-app/src/components/Home.tsx
+++ weather-app/src/components/Home.tsx
@@ -1,7 +1,23 @@
-const Home =()=> {
-    return(
-        <h1>This is homepage</h1>
-    )
-}
+import { Container, Row, Col, Form } from "react-bootstrap";
 
-export default Home
+const Home = () => {
+  return (
+    <Container>
+      <Row className="mt-5 justify-content-center">
+        <h1 className="text-light"><strong>Hello Arian, Discover the weather</strong></h1>
+        <Col xs={12} md={12} lg={8}>
+          <Form>
+            <Form.Group>
+              <Form.Control className="rounded-pill mt-2" type="search" placeholder="Search herre..." />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+          
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
